b280017277eb38a3fb80df71b7db23d0b587b010
jaden0908 <leexingjue0908@gmail.com>
2025-08-11 01:04:25 +0800

    Sign Up

--- mail_config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Use Gmail? Turn on 2-Step Verification -> App passwords -> generate 16-char password
-define('SMTP_HOST', 'smtp.gmail.com');
-define('SMTP_PORT', 587);
-define('SMTP_USER', 'jadenlee09082005@gmail.com');
-define('SMTP_PASS', '');   // 
-define('MAIL_FROM', 'jadenlee09082005@gmail.com'); 
-define('MAIL_FROM_NAME', 'universal studios');
