9d35e87b7a5be4fceaac6a4856128c18528f461c
dmaisarah <dewiimaisarah11@gmail.com>
2025-08-11 09:43:09 +0800

    testing

--- sprint1/mail_config.php
+++ sprint1/mail_config.php
@@ -3,6 +3,6 @@
 define('SMTP_HOST', 'smtp.gmail.com');
 define('SMTP_PORT', 587);
 define('SMTP_USER', 'jadenlee09082005@gmail.com');
-define('SMTP_PASS', '');   // 
+define('SMTP_PASS', 'khqpxhgavslbszkd');   // 
 define('MAIL_FROM', 'jadenlee09082005@gmail.com'); 
 define('MAIL_FROM_NAME', 'universal studios');
