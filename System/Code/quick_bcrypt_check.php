16be3fda39d887a60dae5acc0c60089fb023ffb3
jaden0908 <leexingjue0908@gmail.com>
2025-10-30 16:16:33 +0800

    complete all folder

--- System/Code/quick_bcrypt_check.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo "PHP: ", PHP_VERSION, "<br>";
-$plain = '123456';
-$hash  = password_hash($plain, PASSWORD_BCRYPT);
-echo "hash: ", htmlspecialchars($hash), "<br>";
-echo "verify(123456): ";
-var_dump(password_verify('123456', $hash));
